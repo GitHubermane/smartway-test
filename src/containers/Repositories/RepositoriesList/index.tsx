2481cc193a2df9ad312a2252cfcc8e7e82df62d4
Viktor Kryuchkov <crykour12@gmail.com>
2023-12-21 19:19:09 +0300

    add error to RepositoriesList

--- src/containers/Repositories/RepositoriesList/index.tsx
+++ src/containers/Repositories/RepositoriesList/index.tsx
@@ -1,25 +1,36 @@
 import type { FC } from 'react';
 import type { Repository } from 'types';
+import { RepositoryStore } from 'store';
 import { Item } from './Item';
+import styles from './styles.module.css';
 
 type PropsType = {
   repositories: Repository[];
 };
 
-export const RepositoriesList: FC<PropsType> = ({ repositories }) => (
-  <div>
-    {repositories.map(
-      ({
-        forks_count, stargazers_count, id, owner, html_url, 
-      }) => (
-        <Item
-          key={id}
-          forks={forks_count}
-          stars={stargazers_count}
-          avatar={owner.avatar_url}
-          href={html_url}
-        />
-      ),
-    )}
-  </div>
-);
+export const RepositoriesList: FC<PropsType> = ({ repositories }) => {
+  const { error } = RepositoryStore;
+
+  return (
+    <div className={styles.repostitories_list}>
+      {error ? (
+        <div className={styles.error}>{error.message}</div>
+      ) : (
+        repositories.map(
+          ({
+            forks_count, stargazers_count, id, owner, html_url, 
+          }) => (
+            <Item
+              key={id}
+              id={id}
+              forks={forks_count}
+              stars={stargazers_count}
+              avatar={owner.avatar_url}
+              href={html_url}
+            />
+          ),
+        )
+      )}
+    </div>
+  );
+};
