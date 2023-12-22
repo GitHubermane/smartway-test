0ec6a8b1b12244abdaf4c9949b9e8930f4c09acf
Viktor Kryuchkov <crykour12@gmail.com>
2023-12-22 16:07:21 +0300

    routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
-const App = () => <div>Hello world!</div>;
+import { Layout } from 'components';
+import { Route, Routes } from 'react-router-dom';
+import { routes } from 'routes';
+
+const App = () => (
+  <Layout>
+    <Routes>
+      {routes.map(({ path, Component }) => (
+        <Route
+          key={path}
+          path={path}
+          element={<Component />}
+        />
+      ))}
+    </Routes>
+  </Layout>
+);
 
 export default App;
